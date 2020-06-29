fea703d2db03e78b3c84b436f7d2253484585db9
Adithya R <radithya2002@gmail.com>
2020-06-29 09:19:56 +0530

    ginkgo: Remove ginkgo_vtcamera on extraxt-files
    Change-Id: I7ce141a02afc0247c752ba86a5e82c409b55f822

--- extract-files.sh
+++ extract-files.sh
@@ -28,3 +28,6 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 LINEAGE_ROOT="$MY_DIR"/../../..
 DEVICE_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+
+# Remove vtcamera for ginkgo
+gawk -i inplace '{ p = 1 } /<CameraModuleConfig>/{ t = $0; while (getline > 0) { t = t ORS $0; if (/ginkgo_vtcamera/) p = 0; if (/<\/CameraModuleConfig>/) break } $0 = t } p' "${DEVICE_BLOB_ROOT}/vendor/etc/camera/camera_config.xml"
